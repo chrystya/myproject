68ac9ebf450cabe30ebad49ceab6141e2714219c
chrystya <chrystya88@gmail.com>
2017-09-28 20:41:04 +0300

    login report page test

--- src/parameters/ChromeBrowser.java
+++ src/parameters/ChromeBrowser.java
@@ -1,4 +1,29 @@
 package parameters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class ChromeBrowser {
+
+
+    WebDriver driver = null;
+    public static void main(String [] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\kpred\\Desktop\\sourses\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://mclasshome.aws.wgen.net/reports/Correlation");
+        driver.findElement(By.id("login-username")).clear();
+        driver.findElement(By.id("login-username")).sendKeys("muser47");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("Abcd1234");
+        driver.findElement(By.id("login-submit")).click();
+
+
+
+
+
+    }
+
 }
